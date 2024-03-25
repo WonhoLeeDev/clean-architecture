6c3bcf73d44a59469bfbacb5f019236d9ac94705
WonhoLeeDev <wonholee.dev@gmail.com>
2024-03-26 03:00:32 +0900

    테스트

--- src/test/java/hhplus/cleanarchitecture/service/RegistrationServiceTest.java
+++ src/test/java/hhplus/cleanarchitecture/service/RegistrationServiceTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hhplus.cleanarchitecture.controller.RegistrationDto;
 import hhplus.cleanarchitecture.domain.Registration;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,10 +26,8 @@ class RegistrationServiceTest {
     @Test
     void findRegistrationsTest() throws JsonProcessingException {
         registrationService.register(1L, 3L);
-        List<RegistrationDto> registrations = registrationService.findRegistrationsByUserId(1L);
-        for (RegistrationDto registration : registrations) {
-            System.out.println("registration: " + registration);
-        }
+        registrationService.findRegistrationsByUserId(1L);
+        Assertions.assertThat()
     }
 
 }
